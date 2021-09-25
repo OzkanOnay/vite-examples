1ffd82b0e7e58377004bdfb4e4e49fd7c6d6dfa1
Özkan Onay <ozkanonay@gmail.com>
2021-09-25 11:11:51 +0300

    css examples

--- vite-vanilla-project/main.js
+++ vite-vanilla-project/main.js
@@ -1,9 +1,11 @@
-import './style.css'
+import './style.scss'
+import styles from './example.module.scss'
+
 import { debounce } from 'lodash-es';
 
 console.log(debounce);
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1 class="${styles.title}">Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
